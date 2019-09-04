3382f734b1ad66a38ae5f5318364e8ea369a7e90
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-09-04 15:18:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,6 @@
 def find_element_index(array, value_to_find)
-    if array.include?(value_to_find)
+  if array.include?(value_to_find)
       new_value = array.index(value_to_find)
-    end
   end
   new_value
 end
